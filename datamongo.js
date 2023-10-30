3f98fc502e03d00ac1ef3c7867494ec5da0466c6
CHRNPS <6206021622178@fitm.kmutnb.ac.th>
2023-10-30 23:49:09 +0900

    cange

--- datamongo.js
+++ datamongo.js
@@ -40,7 +40,7 @@ app.get("/products/:id", async (req, res) => {
 app.post("/products", async (req, res) => {
   try {
     const product = await Product.create(req.body);
-    
+
     const newRelation = new ProductCompanyRelation({
       ProductID: product.ProductID, // Use the ID of the newly created product
       CompanyID: req.body.CompanyID, // Assuming CompanyID is available in req.body
@@ -107,7 +107,8 @@ app.post("/companies", async (req, res) => {
     const newRelation = new ProductCompanyRelation({
       ProductID: company.ProductID, // Assuming ProductID is available in req.body
       CompanyID: company.CompanyID, // Use the ID of the newly created company
-    }); newRelation.save();
+    });
+    newRelation.save();
     res.json(company);
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -166,37 +167,38 @@ Product.aggregate([
     console.error(error);
   });
 
-  async function collectDataAndStoreInRelationTable() {
-    try {
-      // Retrieve data from the first table (Product)
-      const productData = await Product.find({}, 'ProductID CompanyID');
-  
-      // Retrieve data from the second table (ProductCompany)
-      const companyData = await ProductCompany.find({}, 'CompanyID');
-  
-      // Process the data and create entries for the ProductCompanyRelation table
-      const relations = [];
-      productData.forEach(product => {
-        companyData.forEach(company => {
-          const relation = {
-            ProductID: product.ProductID,
-            CompanyID: company.CompanyID
-          };
-          relations.push(relation);
-        });
-      });
-  
-      // Insert data into the ProductCompanyRelation table
-      await ProductCompanyRelation.insertMany(relations);
-  
-      console.log('Data inserted into ProductCompanyRelation collection.');
-    } catch (error) {
-      console.error(error);
-    } 
-  }
-  
-  collectDataAndStoreInRelationTable();
+async function collectDataAndStoreInRelationTable() {
+  try {
+    // Retrieve data from the first table (Product)
+    const productData = await Product.find({}, "ProductID CompanyID");
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+    // Retrieve data from the second table (ProductCompany)
+    const companyData = await ProductCompany.find({}, "CompanyID");
+
+    // Process the data and create entries for the ProductCompanyRelation table
+    const relations = [];
+    productData.forEach((product) => {
+      companyData.forEach((company) => {
+        const relation = {
+          ProductID: product.ProductID,
+          CompanyID: company.CompanyID,
+        };
+        relations.push(relation);
+      });
+    });
+
+    // Insert data into the ProductCompanyRelation table
+    await ProductCompanyRelation.insertMany(relations);
+
+    console.log("Data inserted into ProductCompanyRelation collection.");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+collectDataAndStoreInRelationTable();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
