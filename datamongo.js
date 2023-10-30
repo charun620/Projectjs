eff3a39b20723650e03cba998a8d9eda80ed1c1a
CHRNPS <6206021622178@fitm.kmutnb.ac.th>
2023-10-30 23:54:12 +0900

    modified:   datamongo.js

--- datamongo.js
+++ datamongo.js
@@ -200,5 +200,5 @@ collectDataAndStoreInRelationTable();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server  running on http://localhost:${PORT}`);
 });
